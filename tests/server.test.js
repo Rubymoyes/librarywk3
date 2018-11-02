1a55cb732df662602f0e651cc67a9eae94ccb642
Garry <garry@garry.bar>
2018-11-02 14:23:35 +1300

    tests work

--- tests/server.test.js
+++ tests/server.test.js
@@ -11,25 +11,27 @@ jest.mock('../db', () => ({
   ),
 }))
 
-// jest.mock('../db', () => ({
-//   getCheckout: () => Promise.resolve(
-//   {id: 99901, name: 'Search Inside Yourself: The Unexpected Path to Achieving Success, Happiness (and World Peace)', author: 'Chade-Meng Tan'}
-// ),
-// }))
+jest.mock('../db', () => ({
+  getCheckout: () => Promise.resolve(
+  {id: 99901, name: 'Search Inside Yourself: The Unexpected Path to Achieving Success, Happiness (and World Peace)', author: 'Chade-Meng Tan'}
+),
+}))
 
 
 // Basic HTML test
 
-// test('GET /', () => {
-//   return request(server)
-//     .get('/')
-//     .then((res) => {
-//       const $ = cheerio.load(res.text)
-//       const firsth1Text = $('h1').first().text()
-//       expect(firsth1Text).toBe('Books')
-//     })
-//     .catch(err => expect(err).toBeNull())
-// })
+test('GET /', () => {
+  return request(server)
+    .get('/')
+    .then((res) => {
+      const $ = cheerio.load(res.text)
+      const firsth1Text = $('h1').first().text()
+      expect(firsth1Text).toBe('Books')
+    })
+    .catch(err => expect(err).toBeNull())
+})
+
+//Basic HTML test two
 
 
 test('GET /checkout/99901', () => {
@@ -42,3 +44,18 @@ test('GET /checkout/99901', () => {
     })
     // .catch(err => expect(err).toBeNull())
 })
+
+// test("check text id is correct", (done) => {
+//   return request(server)
+//     .get('/checkout/99901').end(function (err, res) {
+//       console.log(res.text)
+//       expect(err).toBeNull()
+//       let expected = 'Checkout Page'
+//       let $ = cheerio.load(res.text)
+//       let actual = $('h1').first().text()
+//       expect(actual).toEqual(expected)
+
+//       done()
+
+//   })
+// }) 
